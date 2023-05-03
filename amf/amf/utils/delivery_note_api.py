7d92104454b03cda0f7a34c7be7e25416dad63e1
Alexandre <alexandre.ringwald@amf.ch>
2023-05-03 14:18:27 +0200

    UPDATE QRCODE 20230503 2.18PM

--- amf/amf/utils/delivery_note_api.py
+++ amf/amf/utils/delivery_note_api.py
@@ -9,6 +9,7 @@ def generate_serial_number_qr_codes(delivery_note):
         if item.serial_no:
             serial_numbers = item.serial_no.split('\n')
             for serial_number in serial_numbers:
-                qr_code_base64 = generate_qr_code(serial_number)
-                qr_codes.append({"serial_number": serial_number, "qr_code": qr_code_base64})
+				if serial_number.strip():
+					qr_code_base64 = generate_qr_code(serial_number)
+					qr_codes.append({"serial_number": serial_number, "qr_code": qr_code_base64})
     return qr_codes
