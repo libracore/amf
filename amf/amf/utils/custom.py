7779daa2263ec4c001638e476778fa48be868d64
aringwald-amf <122607955+aringwald-amf@users.noreply.github.com>
2023-05-15 18:30:15 +0200

    Quick updates

--- amf/amf/utils/custom.py
+++ amf/amf/utils/custom.py
@@ -1,11 +1,25 @@
 import frappe
 
 @frappe.whitelist()
-def get_latest_serial_no(item_code):
+def get_latest_serial_no(so_detail, sales_order, item_code):
     serial_no = frappe.db.sql("""
-        SELECT serial_no FROM `tabStock Entry Detail`
-        WHERE item_code = %s
-        ORDER BY creation DESC
-        LIMIT 1
-    """, item_code, as_dict=1)
+        SELECT 
+            soi.name AS sales_order_item, 
+            (SELECT sed.serial_no 
+            FROM `tabStock Entry` AS se 
+            JOIN `tabStock Entry Detail` AS sed ON se.name = sed.parent
+            WHERE sed.item_code = soi.item_code
+            AND se.work_order = (
+                SELECT wo.name 
+                FROM `tabWork Order` AS wo 
+                WHERE wo.sales_order_item = %s 
+                AND wo.status = 'Completed'
+                LIMIT 1
+            )
+            LIMIT 1
+        ) AS serial_no
+        FROM `tabSales Order` AS so 
+        JOIN `tabSales Order Item` AS soi ON soi.parent = so.name
+        WHERE so.name = %s AND soi.item_code = %s
+    """, (so_detail, sales_order, item_code), as_dict=1)
     return serial_no[0]['serial_no'] if serial_no else None
