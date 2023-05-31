253eb7ee7fc9ba99b5dd1aa9ffb767d2a27ed0ea
lasalesi <larsericmueller@gmail.com>
2023-05-31 19:14:00 +0200

    compatibility

--- amf/www/packaging.py
+++ amf/www/packaging.py
@@ -7,7 +7,7 @@ def get_context(context):
 
 @frappe.whitelist()
 def update_weight(delivery_note, weight, length, height, width, operator):
-    # print(f"Weight in Python: {weight}")  # Print the weight
+    # print("Weight in Python: {weight}".format(weight=weight))  # Print the weight
     delivery = frappe.get_doc("Delivery Note", delivery_note)
     delivery.update({
         "weight": weight,
@@ -22,6 +22,6 @@ def update_weight(delivery_note, weight, length, height, width, operator):
 @frappe.whitelist()
 def upload_image(filename):
     # This is just a placeholder. You'll need to handle the file upload here.
-    print(f"Received file with filename: {filename}")
+    print("Received file with filename: {filename}".format(filename=filename))
     # You might need to use frappe's upload API to handle file uploads
     return "File upload successful"
