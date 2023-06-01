a546e1c9158f218d1c4ac104fc63e19d71159a2b
Alexandre <alexandre.ringwald@amf.ch>
2023-06-01 11:09:42 +0200

    Update on web page img handling

--- amf/www/packaging.py
+++ amf/www/packaging.py
@@ -1,12 +1,15 @@
 import frappe
+import base64
+import os
 from frappe import _
+from frappe.utils.file_manager import save_file
 
 def get_context(context):
     context.no_cache = 1
     context.title = _('Packaging')
 
 @frappe.whitelist()
-def update_weight(delivery_note, weight, length, height, width, operator):
+def update_weight(delivery_note, weight, length, height, width, operator, image_data):
     # print(f"Weight in Python: {weight}")  # Print the weight
     delivery = frappe.get_doc("Delivery Note", delivery_note)
     delivery.update({
@@ -14,10 +17,27 @@ def update_weight(delivery_note, weight, length, height, width, operator):
         "length": length,
         "height": height,
         "width": width,
-        "weight": weight,
         "operator": operator
     })
     delivery.save()
+
+    if image_data:
+        # The image_data is in the format "data:image/png;base64,iVBORw0K...". We need to split out the base64 part:
+        base64_data = image_data.split(",")[-1]
+
+        # Decode the base64 data to bytes:
+        data = base64.b64decode(base64_data)
+
+        # Save the file and link it to the Delivery Note:
+        file = save_file("image.png", data, "Delivery Note", delivery_note, is_private=1)
+
+        # Update the Delivery Note with the file url:
+        delivery.update({
+            "image": file.file_url
+        })
+        delivery.save()
+    
+    delivery.reload()
 
 @frappe.whitelist()
 def upload_image(filename):
