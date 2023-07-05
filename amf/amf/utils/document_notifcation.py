1bf9ab96b2ebefe208136a3e76750faa951e2fe1
Alexandre <alexandre.ringwald@amf.ch>
2023-07-05 13:21:25 +0200

    minor updates

--- amf/amf/utils/document_notifcation.py
+++ amf/amf/utils/document_notifcation.py
@@ -6,15 +6,15 @@ def check_purchase_orders():
     # Fetch all the Purchase Orders which are 'To Bill' and today's date
     purchase_orders = frappe.get_all('Purchase Order', 
         filters = {
-            'status': 'To Bill',
-            'schedule_payment_date': nowdate()  # Today's date
+            'status': 'To Bill'
         }, 
-        fields = ['name', 'owner', 'email_id']  # Assuming 'email_id' field in Purchase Order doctype
+        fields = ['name', 'owner', 'schedule_payment_date']  # Assuming 'email_id' field in Purchase Order doctype
     )
 
     # Loop through the purchase orders and send an email to the owner
     for po in purchase_orders:
-        send_email(po)
+        if po['schedule_payment_date'] == nowdate():
+            send_email(po)
 
 def send_email(purchase_order):
     # Creating email context
@@ -23,7 +23,8 @@ def send_email(purchase_order):
         'message': "Dear {purchase_order['owner']},\n\nThe Purchase Order '{purchase_order['name']}' is due for billing soon. Please proceed with the payment or send the invoice to the office manager.\n\nThanks,".format(),
         'subject': "Automatic Notification for Purchase Order: {purchase_order['name']}".format(),
         'communication_medium': 'Email',
-        'send_email': True
+        'send_email': True,
+        'cc': ['madeleine.fryer@amf.ch']
     }
 
     # Sending email
