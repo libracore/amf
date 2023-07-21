146e603c2bdc22d5e307306eb4534a1b9b861e1a
Alexandre <alexandre.ringwald@amf.ch>
2023-07-21 17:17:38 +0200

    update planif

--- amf/www/planification.py
+++ amf/www/planification.py
@@ -3,5 +3,5 @@ import frappe
 @frappe.whitelist()
 def get_planning_list():
     print("get_planning_list")
-    planning_list = frappe.get_all('Planning', fields=['name', 'status', 'job_card', 'qty', 'item'])
+    planning_list = frappe.get_all('Planning', fields=['name', 'status', 'job_card', 'qty', 'item', 'project', 'who', 'created_on', 'planned_for', 'end_date', 'delivered_qty', 'material', 'drawing', 'program', 'comments', 'filter'])
     return planning_list
