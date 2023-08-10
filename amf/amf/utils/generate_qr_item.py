ef0b9bd3e837c902bf865f37bd3bd9b12fe29e91
Alexandre <alexandre.ringwald@amf.ch>
2023-08-10 10:22:33 +0200

    Minor Updates

--- amf/amf/utils/generate_qr_item.py
+++ amf/amf/utils/generate_qr_item.py
@@ -1,3 +1,4 @@
+import base64
 import frappe
 from frappe import _
 from frappe.utils.file_manager import save_file, remove_all
@@ -8,8 +9,13 @@ import os
 def generate_and_attach_qrcode():
     # Fetch all items
     items = frappe.get_all('Item', filters={"name": ["NOT LIKE", "%/%"]}, fields=['name'])
-
+    
+    total_items = len(items)
+    processed = 0
+    
     for item in items:
+        remove_all("Item", item.name, False)
+
         item_name = item.name
         qr = qrcode.QRCode(
             version=1,
@@ -25,32 +31,19 @@ def generate_and_attach_qrcode():
         # Convert the image to bytes
         buffered = BytesIO()
         img.save(buffered, format="PNG")
-        img_content = buffered.getvalue()
+        img_content = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-        # Save the generated QR code to a PNG
-        # file_path = "/tmp/qr_code_{0}.png".format(item_name)
-        # img.save(file_path)
+        processed += 1
+        progress = (processed / total_items) * 100
+        frappe.publish_realtime("progress", {"progress": progress}, user=frappe.session.user)
 
         # Attach QR code PNG to the Item's qr_code field
         attach_to_item(item_name, img_content)
 
 def attach_to_item(item_name, img_content):
     try:
-        # Get the existing QR code URL
-        # existing_qr_code = frappe.db.get_value('Item', item_name, 'qr_code')
-
-        # Remove the QR code field value
-        # frappe.db.set_value("Item", item_name, "qr_code", None)
-
-        # If there's an existing QR code, delete it
-        # if existing_qr_code:
-        #     existing_file = frappe.get_list("File", filters={"file_url": existing_qr_code}, fields=["name"])
-        #     if existing_file:
-        #         frappe.delete_doc("File", existing_file[0].name)
-
         # Create a new File attachment
-        remove_all("Item", item_name, True)
-        file_data = save_file(f"{item_name}.png", img_content, "Item", item_name, "Home/Attachments", is_private=1)
+        file_data = save_file("{}.png".format(item_name), img_content, "Item", item_name, is_private=1)
         if file_data:
             # Update the Item's qr_code field with the attached file's URL
             frappe.db.set_value("Item", item_name, "qr_code", file_data.file_url)
--- amf/www/generate_qrcode.py
+++ amf/www/generate_qrcode.py
@@ -5,9 +5,8 @@ from amf.amf.utils.generate_qr_item import generate_and_attach_qrcode  # Adjust 
 
 @frappe.whitelist()
 def generate_and_attach():
-    frappe.msgprint("generate_and_attach")
     try:
         generate_and_attach_qrcode()  # Adjust this call accordingly
-        return "QR Codes generated successfully."
+        return "QR Codes Generated Successfully."
     except Exception as e:
         return str(e)
