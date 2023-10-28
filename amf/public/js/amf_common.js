2bba912bf289010a0d782d2593049e40256204b3
aringwald-amf <122607955+aringwald-amf@users.noreply.github.com>
2023-10-28 09:48:48 +0200

    Update amf_common.js

--- amf/public/js/amf_common.js
+++ amf/public/js/amf_common.js
@@ -29,9 +29,12 @@ window.onload = async function () {
         if (navlogo.length > 0) {
             if (window.location.hostname.includes("amf")) {
                 navlogo[0].src = "https://amf.libracore.ch/files/AMF2023.png";
-                //navlogo[0].style.width = "45px";  // Change width to 45px
                 navlogo[0].style.marginTop = "1";
                 navlogo[0].setAttribute('style', 'width: 50px !important');
+                //navlogo[0].src = "https://amf.libracore.ch/files/AMF2023-Full.png";
+                //navlogo[0].style.marginTop = "-1";
+                //navlogo[0].style.marginLeft = "2";
+                //navlogo[0].setAttribute('style', 'width: 150px !important');
             }
         }
         var navsearch = document.getElementById("navbar-search");
