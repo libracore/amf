78b344a1e912428822eb4059200278fa371df4ee
Alexandre <alexandre.ringwald@amf.ch>
2024-02-01 06:57:39 +0100

    Cleaning

--- amf/amf/utils/safety_stock_check.py
+++ amf/amf/utils/safety_stock_check.py
@@ -4,63 +4,6 @@ import math
 import datetime
 import statistics
 
-
-def update_safety_stock_and_check_levels():
-    # Get the current year and calculate last year's dates
-    current_year = datetime.datetime.now().year
-    last_year_start = datetime.date(current_year - 1, 1, 1)
-    last_year_end = datetime.date(current_year - 1, 12, 31)
-
-    items = frappe.get_all("Item", filters={'is_stock_item': 1, 'disabled': 0}, fields=["name", "safety_stock", "reorder"])
-    for item in items:
-        # Fetch total outflow for this item for the last year
-        total_outflow = frappe.db.sql(
-            """
-            SELECT SUM(actual_qty)
-            FROM `tabStock Ledger Entry`
-            WHERE item_code = %s
-            AND posting_date BETWEEN %s AND %s
-            AND actual_qty < 0
-        """,
-            (item["name"], last_year_start, last_year_end),
-        )
-        # The SQL query returns a list of tuples, so we need to extract the actual value
-        total_outflow = (
-            total_outflow[0][0] if total_outflow and total_outflow[0][0] else 0
-        )
-
-        # Calculate safety stock as 25% more than the average monthly outflow
-        average_monthly_outflow = abs(total_outflow / 12)
-        safety_stock = math.ceil(average_monthly_outflow * 1.25)  # Add 25% buffer
-        # Update safety stock value in Item doctype
-        frappe.db.set_value("Item", item["name"], "safety_stock", safety_stock)
-
-        # Now let's check the stock levels against this new safety stock
-        highest_stock = 0  # Initialize variable to store the highest stock value
-        all_warehouses = frappe.get_all("Warehouse")
-        filtered_warehouses = [wh for wh in all_warehouses if "AMF_OLD" not in wh.name]
-
-        for warehouse in filtered_warehouses:
-            current_stock = (
-                frappe.db.get_value(
-                    "Bin",
-                    {"item_code": item["name"], "warehouse": warehouse.name},
-                    "actual_qty",
-                )
-                or 0
-            )
-            # Update the highest stock value if the current stock is higher
-            if current_stock > highest_stock:
-                highest_stock = current_stock
-
-        if highest_stock < item["safety_stock"]:
-            # Set the "Reorder" checkbox to True (checked)
-            frappe.db.set_value("Item", item["name"], "reorder", 1)
-            print(
-                f"Setting 'reorder' to 1 / Item: {item['name']} / Stock Value = {highest_stock} / Safety Stock = {item['safety_stock']}"
-            )
-
-
 def check_stock_levels():
     # Constants
     Z = 1.64  # Z-score for 95% service level
@@ -242,33 +185,3 @@ def sendmail(items):
     # Creating communication and sending email
     comm = make(**email_context)
     return comm
-    
-    """ Archives...
-    print("sendmail")
-    # Creating email context
-    email_context = {
-        'recipients': 'alexandre.ringwald@amf.ch',
-        'content': f"<p>Item {name} has reached Reorder Level. Please take necessary actions.</p>",
-        'subject': f"Running Low on {name}",
-        'doctype': 'Item',
-        'name': name,
-        'communication_medium': 'Email',
-        'send_email': True,
-        'attachments': attachments or [],
-    }
-    
-    # Creating communication and sending email
-    comm = make(**email_context)
-    
-    return comm
-
-    # email_args = {
-    #     'recipients': 'alexandre.ringwald@amf.ch',
-    #     'message': f"<p>Item {name} has reached Reorder Level. Please take necessary actions.</p>",
-    #     'subject': f"Running Low on {name}",
-    #     'reference_doctype': 'Item',
-    #     'reference_name': name,
-    # }
-    # if attachments:email_args['attachments']=attachments
-    # #send mail
-    # frappe.enqueue(method=frappe.sendmail, queue='short', timeout=300, **email_args) """
