5c8df53365fbd587b6dae51c27aaf4c924478ca2
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2024-05-28 14:07:34 +0000

    Update tracking.py

--- amf/www/tracking.py
+++ amf/www/tracking.py
@@ -36,7 +36,7 @@ def get_tracking_numbers():
 
 def get_tracking_info(tracking_number):
     
-    API_KEY = frappe.db.get_single_value("AMF Global Settings", "dhl_api_key")
+    API_KEY = frappe.db.get_single_value("AMF DHL Settings", "dhl_api_key")
 
     params = urllib.parse.urlencode({
         'trackingNumber': tracking_number,
