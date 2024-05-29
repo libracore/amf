5e74ecd7d5796bf3f27d47a64cc1538002b9a75c
Alexandre <alexandre.ringwald@amf.ch>
2024-05-29 09:21:18 +0200

    Tracking Update

---
+++ amf/amf/doctype/amf_dhl_settings/amf_dhl_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, libracore AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AMF DHL Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
