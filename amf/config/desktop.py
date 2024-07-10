759c52203f1b9c994274a1508c40a1e4fd80e3b9
lasalesi <larsericmueller@gmail.com>
2024-07-10 13:24:32 +0200

    adding desktop entry

--- amf/config/desktop.py
+++ amf/config/desktop.py
@@ -3,12 +3,19 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-	return [
-		{
-			"module_name": "AMF",
-			"color": "#2b47d9",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("AMF")
-		}
-	]
+    return [
+        {
+            "module_name": "AMF",
+            "color": "#2b47d9",
+            "icon": "octicon octicon-file-directory",
+            "type": "module",
+            "label": _("AMF")
+        },
+        {
+            "module_name": "Master CRM",
+            "color": "#EF4DB6",
+            "icon": "icon crm-blue",
+            "type": "module",
+            "label": _("Master CRM")
+        }
+    ]
