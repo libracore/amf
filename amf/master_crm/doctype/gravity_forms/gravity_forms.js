40c9e8dcad5a1346ba10e637cc9b678374b108e1
lasalesi <larsericmueller@gmail.com>
2024-09-04 16:03:15 +0200

    gravity import workflow

--- amf/master_crm/doctype/gravity_forms/gravity_forms.js
+++ amf/master_crm/doctype/gravity_forms/gravity_forms.js
@@ -3,6 +3,35 @@
 
 frappe.ui.form.on('Gravity Forms', {
     refresh: function(frm) {
-
+        if ((frm.doc.gravity_host) && (frm.doc.gravity_key) && (frm.doc.gravity_secret)) {
+            frm.add_custom_button(__("Fetch Forms"), function() {
+                fetch_forms(frm);
+            });
+            frm.add_custom_button(__("Fetch Entries"), function() {
+                fetch_entries(frm);
+            });
+        }
     }
 });
+
+function fetch_forms(frm) {
+    frappe.call({
+        'method': 'amf.master_crm.doctype.gravity_forms.gravity_forms.fetch_forms',
+        'freeze': true,
+        'freeze_message': __("Syncing forms... Please wait..."),
+        'callback': function(response) {
+            frappe.show_alert( __("Forms updated") );
+        }
+    });
+}
+
+function fetch_entries(frm) {
+    frappe.call({
+        'method': 'amf.master_crm.doctype.gravity_forms.gravity_forms.fetch_entries',
+        'freeze': true,
+        'freeze_message': __("Syncing entries... Please wait..."),
+        'callback': function(response) {
+            frappe.show_alert( __("Entries updated") );
+        }
+    });
+}
--- amf/master_crm/doctype/gravity_forms/gravity_forms.py
+++ amf/master_crm/doctype/gravity_forms/gravity_forms.py
@@ -8,6 +8,8 @@ from frappe.model.document import Document
 import requests
 from requests.auth import HTTPBasicAuth
 from frappe.utils.password import get_decrypted_password
+import json
+from frappe.utils import cint
 
 API_PATH = "/wp-json/gf/v2/"
 
@@ -61,7 +63,7 @@ class GravityForms(Document):
         return
     
     def get_form_entries(self, form_id):
-        endpoint = "{0}{1}forms/{2}/entries".format(self.gravity_host, API_PATH, form_id)
+        endpoint = "{0}{1}forms/{2}/entries?sorting[key]=id&sorting[direction]=ASC&sorting[is_numeric]=true&paging[page_size]=100".format(self.gravity_host, API_PATH, form_id)
 
         response = requests.get(endpoint, auth=self.get_auth())
         
@@ -73,3 +75,68 @@ class GravityForms(Document):
             frappe.throw("Gravity Forms: Error {0}: {1}".format(response.status_code, response.text))
             
         return entries
+
+@frappe.whitelist()
+def fetch_forms():
+    gravity = frappe.get_doc("Gravity Forms", "Gravity Forms")
+    forms = gravity.get_forms()
+    if forms:
+        for f in forms:
+            if not frappe.db.exists("Gravity Form", f.get('id')):
+                fields = gravity.get_form_fields(f.get('id'))
+                new_form = frappe.get_doc({
+                    'doctype': 'Gravity Form',
+                    'gravity_form_id': f.get('id'),
+                    'title': f.get('title')
+                })
+                for i in fields:
+                    new_form.append("fields", {
+                        'field_id': i.get('id'),
+                        'field_name': i.get('label')
+                    })
+                new_form.insert(ignore_permissions=True)
+        frappe.db.commit()
+        
+    return forms
+
+@frappe.whitelist()
+def fetch_entries():
+    # first, make sure all forms are available
+    forms = fetch_forms()
+    gravity = frappe.get_doc("Gravity Forms", "Gravity Forms")
+    if forms:
+        for f in forms:
+            if cint(frappe.get_value("Gravity Form", f.get('id'), 'disabled')) == 0:
+                frappe.log_error("Fetch {0} entries".format(f.get('id')))
+                entries = fetch_form_entries(f.get('id'))
+        
+    return
+
+@frappe.whitelist()
+def fetch_form_entries(gravity_form):
+    gravity = frappe.get_doc("Gravity Forms", "Gravity Forms")
+    entries = gravity.get_form_entries(gravity_form)
+    # find import table
+    form_doc = frappe.get_doc("Gravity Form", gravity_form)
+    import_keys = {}
+    for f in form_doc.fields:
+        if cint(f.get('import')) == 1:
+            import_keys[f.get('field_id')] = f.get('field_name')
+    if entries:
+        for e in entries:
+            if not frappe.db.exists("Gravity Form Entry", e.get('id')):
+                new_entry = frappe.get_doc({
+                    'doctype': 'Gravity Form Entry',
+                    'gravity_form_entry_id': e.get('id'),
+                    'content': "{0}".format(e),
+                    'gravity_form': gravity_form
+                })
+                for k, v in import_keys.items():
+                    new_entry.append('fields', {
+                        'field_name': v,
+                        'value': e.get(k)
+                    })
+                new_entry.insert(ignore_permissions=True)
+        frappe.db.commit()
+        
+    return entries
