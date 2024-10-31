00f1c277ca63d02cbc30be248689fc0f4b7b510a
Alexandre <alexandre.ringwald@amf.ch>
2024-10-31 16:13:11 +0100

    updates contact.py

--- amf/amf/utils/contact.py
+++ amf/amf/utils/contact.py
@@ -60,7 +60,7 @@ def update_contact_status():
 def update_contact_products():
     try:
         # Fetch all contacts
-        contacts = frappe.get_all("Contact", fields=["name"])
+        contacts = frappe.get_all("Contact", {"status": "Customer"}, ["name"])
 
         if not contacts:
             frappe.msgprint("No contacts found.")
@@ -85,22 +85,25 @@ def update_contact_products():
                     for item in sales_order_items:
                         item_code = item["item_code"]
                         item_quantities[item_code] = item_quantities.get(item_code, 0) + item["qty"]
-                        print(item_quantities, "for contact:", contact.name)
+                        #print(item_quantities, "for contact:", contact.name)
 
                 # Update the 'contact_product' child table
                 for item_code, qty in item_quantities.items():
+                    # Fetch item name once per item_code
+                    item_name = frappe.get_value("Item", item_code, "item_name")
                     # Check if item_code already exists in the contact's product table
-                    existing_product = frappe.get_value("contact_product", {"parent": contact.name, "parentfield": "products", "item_code": item_code}, ["name", "quantity"])
-
+                    existing_product = frappe.get_all("contact_product", {"parent": contact.name, "parentfield": "products", "item_code": item_code}, ["name", "quantity"])
+                    
                     if existing_product:
                         # Update the existing row's quantity
-                        new_qty = existing_product[1] + qty  # `quantity` is second in `existing_product`
-                        frappe.db.set_value("contact_product", existing_product[0], "quantity", new_qty)
+                        new_qty = int(existing_product[0]['quantity']) + int(qty)  # `quantity` is second in `existing_product`
+                        frappe.db.set_value("contact_product", existing_product[0]['name'], "quantity", new_qty)
                     else:
                         # Add a new row in the contact's product child table
                         contact_doc = frappe.get_doc("Contact", contact.name)
                         contact_doc.append("products", {
                             "item_code": item_code,
+                            "item_name": item_name,
                             "quantity": qty
                         })
                         contact_doc.save(ignore_permissions=True)
