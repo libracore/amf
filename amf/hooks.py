01bc9d0a53bae41807f1202fe7c07d779936584e
Alexandre <alexandre.ringwald@amf.ch>
2024-11-04 16:19:45 +0100

    Various Updates

--- amf/hooks.py
+++ amf/hooks.py
@@ -48,7 +48,10 @@ doctype_list_js = {
 
 doc_events = {
     "Work Order": {
-        "on_submit": "amf.amf.utils.on_work_order_submit.on_submit_wo"
+        "on_submit": [
+            "amf.amf.utils.custom.qr_code_to_document",
+            "amf.amf.utils.on_work_order_submit.on_submit_wo",
+        ]
     },
     "Job Card": {
         "on_submit": "amf.amf.utils.custom.qr_code_to_document"
@@ -61,6 +64,9 @@ doc_events = {
     },
     "Stock Entry": {
         "before_save": "amf.www.fftest.update_rate_and_availability_ste"
+    },
+    "Stock Entry": {
+        "on_submit": "amf.amf.utils.custom.qr_code_to_document"
     },
     "Production Order": {
         "before_submit": "amf.amf.utils.custom.attach_qr_code_to_document"
