900f687621773a3771af5c7d6c20f10a884c3e6a
Alexandre <alexandre.ringwald@amf.ch>
2025-03-24 14:04:14 +0100

    Updates on CRM

--- amf/master_crm/doctype/customer_satisfaction_survey/customer_satisfaction_survey.js
+++ amf/master_crm/doctype/customer_satisfaction_survey/customer_satisfaction_survey.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer Satisfaction Survey', {
+    refresh: function(frm) {
+        set_item_queries(frm)
+    },
 	before_submit: function(frm) {
         if (frm.doc.contact_name && frm.doc.organization_name) {
             frm.set_value("title", frm.doc.contact_name + " for " + frm.doc.organization_name);
@@ -18,3 +21,11 @@ frappe.ui.form.on('Customer Satisfaction Survey', {
         }
     },
 });
+
+function set_item_queries(frm) {
+    frm.set_query("contact_person", () => ({
+        filters: [
+            ['Contact', 'company_name', 'Like', frm.doc.customer],
+        ],
+    }));
+}
