6f5cf58c4f12b90e0bae986ec005196240f7b1f8
Alexandre <alexandre.ringwald@amf.ch>
2025-05-16 11:46:26 +0200

    custo report

--- amf/config/master_crm.py
+++ amf/config/master_crm.py
@@ -67,6 +67,13 @@ def get_data():
                         "label": _("Sales Analytics"),
                         "doctype": "Sales Invoice",
                         "is_query_report": True
+                    },
+                    {
+                        "type": "report",
+                        "name": "Customer Satisfaction",
+                        "label": _("Customer Satisfaction"),
+                        "doctype": "Customer Satisfaction Survey",
+                        "is_query_report": True
                     }
             ]
         },
