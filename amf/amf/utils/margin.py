0fc12c3f58685da14a09566159ff2d7dc51b2298
Lars <lars.mueller@libracore.com>
2025-08-20 18:44:53 +0000

    Edit margin.py

--- amf/amf/utils/margin.py
+++ amf/amf/utils/margin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2024, [Your Company Name] and contributors
+# Copyright (c) 2024-2025, AMF and contributors
 # For license information, please see license.txt
 
 import frappe
