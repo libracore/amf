bb368765f8253936b5bcf3e80cc1f1d9434066ac
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2025-08-25 11:59:48 +0200

    Updates

--- amf/public/js/item.js
+++ amf/public/js/item.js
@@ -2,18 +2,26 @@ function suggest_item_code(frm) {
     // Only do this if the document is new and we have both item_group + item_type
     if (frm.is_new() && frm.doc.item_group && frm.doc.item_type) {
         frappe.call({
-            method: 'amf.amf.utils.item_master.get_max_six_digit_item_code',
-            args: {
-                item_group: frm.doc.item_group,
-                item_type: frm.doc.item_type
-            },
+            method: 'amf.amf.doctype.item_creation.item_creation.get_last_item_code',
             callback: function(r) {
                 if (!r.exc && r.message) {
-                    let highest_code = parseInt(r.message, 10) || 0;
-                    let next_code = highest_code + 1;
-                    // Convert the numeric value back into 6 digits, zero-padded
-                    let padded = String(next_code).padStart(6, '0');
-                    frm.set_value('item_code', padded);
+                    switch (frm.doc.item_group) {
+                        case "Valve Head":
+                            frm.set_value('item_code', '300' + r.message);
+                            break;
+                        case "Valve Seat":
+                            if (frm.doc.item_type == 'Component')
+                                frm.set_value('item_code', '200' + r.message);
+                            else if (frm.doc.item_type == 'Sub-Assembly')
+                                frm.set_value('item_code', '210' + r.message);
+                            break;
+                        case "Plug":
+                            if (frm.doc.item_type == 'Component')
+                                frm.set_value('item_code', '100' + r.message);
+                            else if (frm.doc.item_type == 'Sub-Assembly')
+                                frm.set_value('item_code', '110' + r.message);
+                            break;
+                    }
                 }
             }
         });
