000126b7c081355ade9c92bb732202b21db46d2e
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2025-11-25 09:23:22 +0100

    update production-timer

--- amf/www/production-timer.py
+++ amf/www/production-timer.py
@@ -300,7 +300,9 @@ def add_operator(work_order, operator):
     """
     _ensure_logged_in()
     
+    print("Adding operator:", operator)
     operator = operator.upper()
+
     
     # check if operator exist
     if not frappe.db.get_all("User", {"username": operator}, "name"):
@@ -464,7 +466,7 @@ def clean_assignement(trigram):
             for sess in reversed(timer.sessions_list):
                 
                 if sess.operator == trigram and sess.stop_time is None:
-                    frappe.msgprint("Vous travaillez actuellement sur le timer: {}, mise en pause de la session liée à votre trigramme".format(timer.name))
+                    # frappe.msgprint("Vous travaillez actuellement sur le timer: {}, mise en pause de la session liée à votre trigramme".format(timer.name))
                     sess.stop_time = now
                     sess.comment = "Auto-pause avant nettoyage assignation"
                     #sess.operator = trigram
