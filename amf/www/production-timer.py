76ce33c9726fd2e16e87a455fb360097d563b02e
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2025-12-03 10:05:53 +0100

    Enhance clean_assignment function to check work order status before finishing timers

--- amf/www/production-timer.py
+++ amf/www/production-timer.py
@@ -300,7 +300,6 @@ def add_operator(work_order, operator):
     """
     _ensure_logged_in()
     
-    print("Adding operator:", operator)
     operator = operator.upper()
 
     
@@ -453,10 +452,18 @@ def clean_assignement(trigram):
         filters={
             "assigned_operators": ["like", f"%{trigram}%"],
             "status": ["in", ["PAUSED", "IN PROCESS"]]},
-        fields=["name"]
+        fields=["name", "work_order"]
     )
 
     for t in timers:
+        # check if work_order status is not Completed, Stopped or Cancelled
+        of_status = frappe.db.get_value("Work Order", t["work_order"], "status")
+        if of_status in ["Completed", "Stopped", "Cancelled"]:
+            finish_timer(t["work_order"])
+            continue
+
+
+
         timer = frappe.get_doc("Timer Production", t["name"])
         
         if timer.status == "IN PROCESS":
