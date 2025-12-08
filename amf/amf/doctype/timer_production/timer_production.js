2edbb0f21fad5ca4fb05973b67d404c135fbfb49
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2025-12-08 10:06:05 +0100

    Refactor refresh function in Timer Production to improve status indicator handling

--- amf/amf/doctype/timer_production/timer_production.js
+++ amf/amf/doctype/timer_production/timer_production.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Timer Production', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		if (!frm.doc.status) return;
 
-	// }
+		if (frm.doc.status === 'IN PROCESS') {
+			frm.page.set_indicator(__('In Process'), 'orange');
+		} else if (frm.doc.status === 'FINISHED') {
+			frm.page.set_indicator(__('Finished'), 'green');
+		} else if (frm.doc.status === 'PAUSED') {
+			frm.page.set_indicator(__('Paused'), 'blue');
+		} else {
+			frm.page.set_indicator(__('Unknown'), 'gray');
+		}
+	}
 });
+
