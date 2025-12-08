a6864f7edf37435ab4f362b591fa33f40427ab82
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2025-12-08 15:05:37 +0100

    Add HS Code doctype and auto-complation of the customs tariff number

---
+++ amf/amf/doctype/hs_code/hs_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, libracore AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('HS Code', {
+	// refresh: function(frm) {
+
+	// }
+});
