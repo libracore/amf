bab0e67a74d89cfb89f88af6c44473e32a163202
Lars <lars.mueller@libracore.com>
2026-08-19 21:12:14 +0000

    Patch conflict resolved

--- amf/patches/v12_0/update_product_descriptions_2026_08_19.py
+++ amf/patches/v12_0/update_product_descriptions_2026_08_19.py
@@ -8,6 +8,7 @@ from amf.amf.utils.product_description_update_2026 import apply_product_descript
 def execute():
 	frappe.set_user("Administrator")
 	summary = apply_product_description_updates(frappe)
-	frappe.logger("amf.patches").info(
-		"Updated Product descriptions: {applied_count} changed, {product_count} verified".format(**summary)
-	)
+	# note: no permission to write this
+	#frappe.logger("amf.patches").info(
+	#	"Updated Product descriptions: {applied_count} changed, {product_count} verified".format(**summary)
+	#)
