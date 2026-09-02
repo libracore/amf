4d9bc7c598a446387befcfe7b5129aa6870be17c
Alexandre Ringwald <alexandre.ringwald@amf.ch>
2026-09-02 16:44:32 +0200

    Print Format

---
+++ amf/patches/v12_0/install_standard_branding_amf_2026.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from amf.amf.utils.sales_order_print_format import install_sales_order_print_format
+
+
+def execute():
+    install_sales_order_print_format()
